9a0b02b1b35f0889144997e583ac2bae1a68a725
Jzedy <Jzedy@foxmail.com>
2019-03-27 21:54:05 +0800

    leetcode-cn 005

---
+++ src/main/java/com/cn/jzedy/Solution_005_LongestPalindromicSubstring.java
@@ -0,0 +1,40 @@
+package com.cn.jzedy;
+
+import org.junit.Test;
+
+/**
+ * @author Jzedy
+ * @time 2019/3/27 21:16
+ * @description
+ */
+public class Solution_005_LongestPalindromicSubstring {
+
+    @Test
+    public void test(){
+        char [] chars = {'a','b','a'};
+        System.out.println(longestPalindrome("abdcac"));
+    }
+
+    public String longestPalindrome(String s) {
+        if (s == null || s.trim().length() == 0) return s;
+        char[] chars = s.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            for (int j = 0; j <= i; j++) {
+                if (check(chars,j,chars.length-1+j-i)){
+                    return String.valueOf(chars).substring(j,chars.length+j-i);
+                }
+            }
+
+        }
+        return String.valueOf(chars[0]);
+    }
+
+    private boolean check(char[] chars,int start,int end){
+        for (;start <= end;start++,end--){
+            if (chars[start] != chars[end]){
+                return false;
+            }
+        }
+        return true;
+    }
+}
